67831ce5ff2c22c7c9c5fd46f9e9a0be13afc951
Paulo Silva <engelik@gmail.com>
2015-12-11 12:52:09 +0000

    desktop menu done, needed to change some breakpoints to acomodate for it

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -25,19 +25,71 @@ var main = function () {
         }, 1000);
         e.preventDefault();
         if($anchor.hasClass('menu-btn')){
-          menuToggle();
+           // only if on hamburger mode
+          if(window.innerWidth < 911) {
+            menuToggle();
+          }
         }
   });
   
   // back top arrow
   $(document).scroll(function(){
-    if($('body').scrollTop() > 350) {
+    if($('body').scrollTop() > 160) {
       $('.back-top').fadeIn(400);
     }
     else {
       $('.back-top').fadeOut(400);      
     }
   });
+  
+  // change menu to fixed on scroll 
+  var toogleMenuFixed = function(height) {
+    if($('body').scrollTop() > height){
+          $('nav').addClass('fixed');
+        } else {
+          $('nav').removeClass('fixed');        
+        }
+  };
+  
+  $(document).scroll(function(){
+    var windowWidth = window.innerWidth;
+    // only if not on hamburger mode
+    if(windowWidth > 910) {
+      // diferent breakpoints
+      if(windowWidth > 1180) {
+        toogleMenuFixed(260);
+      } else if (windowWidth >1010){
+        toogleMenuFixed(160);
+      } else {
+        toogleMenuFixed(110);
+      }
+    }
+  });
+  
+  // active button on menu based on scroll
+  var sections = ['#about', '#objectives', '#tech-skills', '#projects', '#education', '#experience', '#contact' ];
+  $(document).scroll(function(){
+      var sectionOffset = [];
+      // getting all the offsets
+      // minus a margin value (so it activates before going offscreen)
+      // activates when title is on the top 1/5 of the screen
+      var offsetMargin = -(window.innerHeight/5);
+      for(var i=0, max=sections.length;i<max-1;i++){
+        sectionOffset.push($(sections[i]).offset().top + offsetMargin);
+      }
+      //last one is special 
+      //needs to give have margin since he will never cross top screen      
+      sectionOffset.push($(sections[sections.length-1]).offset().top - window.innerHeight + $('#contact').height());      
+      
+      // looking for the active section
+      for(var i=0, max=sections.length;i<max;i++){
+        if(sectionOffset[i] < $('body').scrollTop()) {
+          $('.menu-btn').removeClass('active');
+          $($('.menu-btn')[i]).addClass('active');
+        }
+      }
+      
+    });
   
   // menu toggle function
   var menuToggle = function(){
@@ -47,7 +99,7 @@ var main = function () {
           width: ($('body').width()-20)+'px'
         }, 200, function(){
           menu.animate({ 
-          height: '284px'
+          height: '325px'
         }, 300);
         });
       menu.addClass('menu-open');
@@ -71,6 +123,4 @@ var main = function () {
   });
 };
 
-	// width: 180px;
-	// height: 286px;
 $(document).ready(main);
