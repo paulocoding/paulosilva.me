89a709665f094f5472395e472e80b1dc581b5fbb
Paulo Silva <engelik@gmail.com>
2017-01-19 19:45:44 +0000

    refactored sass to unify primary color

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,19 @@ var gulp  = require('gulp'),
     browserSync = require('browser-sync').create(),
     autoprefixer = require('gulp-autoprefixer'),
     concat = require('gulp-concat');
-    
+
 // define the default task and add the watch task to it
 gulp.task('default', ['watch']);
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
-    
+
     browserSync.init({
         server: "./public/"
     });
-    
+
     gulp.watch('source/js/**/*.js', ['concatJS']);
-    gulp.watch('source/scss/**/*.scss', ['sass']);    
+    gulp.watch('source/scss/**/*.scss', ['sass']);
     gulp.watch(['./public/*.html','./public/assets/css/*.css','./public/assets/js/*.js']).on('change', browserSync.reload);
 });
 
@@ -34,14 +34,14 @@ gulp.task('browser-sync', function() {
 
 //  configure the sass compiler task
 gulp.task('sass', function() {
-    gulp.src('source/scss/**/*.scss')
+    gulp.src('source/scss/styles.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(gulp.dest('./public/assets/css/'));
 });
 
 // concatinating js files
- 
+
 gulp.task('concatJS', function() {
   return gulp.src('./source/js/*.js')
     .pipe(concat('bundle.js'))
